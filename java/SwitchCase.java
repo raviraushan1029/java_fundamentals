6c97bdc8239fdbac80c7576180028b6eb2dea17e
Ravi Raushan <88451250+raviraushan1029@users.noreply.github.com>
2025-08-14 14:03:26 +0530

    Create SwitchCase.java

---
+++ java/SwitchCase.java
@@ -0,0 +1,22 @@
+
+public class SwitchCase {
+
+	
+	public static void main(String[] args) {
+		
+		
+		int age=1;
+		
+		switch(age){
+		
+		case 0:
+			System.out.println("hello");
+			break;
+		case 1:
+			System.out.println("hello guys");
+			break;
+		default:
+			System.out.println("my age is "+ age);
+		}
+	}
+}
