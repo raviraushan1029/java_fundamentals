097525d725e713b00f0db0e9fe651b7777160f38
Ravi Raushan <88451250+raviraushan1029@users.noreply.github.com>
2025-08-14 14:01:36 +0530

    Create Methods.java

---
+++ java/Methods.java
@@ -0,0 +1,28 @@
+
+class GFG{
+	
+	  int sum=0;
+	   int add(int x,int y){
+		sum=x+y;
+		return sum;
+	  }
+}
+
+
+public class Methods {
+
+	
+	public static void main(String[] args) {
+		
+		GFG g =new GFG();
+		int s=g.add(8, 12);
+		
+		
+		System.out.println("Sum of two strings is "+s);
+	}  
+		
+		
+	
+		
+	}
+
